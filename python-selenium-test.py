8f475712c8b91bb8a645d77ef4e57c48f4e4ad2a
Adrian Pastor <6169628+pagvac@users.noreply.github.com>
2024-05-15 22:57:11 -0400

    Update python-selenium-test.py

--- python-selenium-test.py
+++ python-selenium-test.py
@@ -11,20 +11,40 @@ from selenium.webdriver.support import expected_conditions as EC
 #display = Display(visible=0, size=(800, 800))  
 #display.start()
 
-chromedriver_autoinstaller.install() 
+chromedriver_autoinstaller.install()
+
+chrome_options = webdriver.ChromeOptions()    
+# Add your options as needed    
+options = [
+  # Define window size here
+   "--window-size=1200,1200",
+    "--ignore-certificate-errors"
+ 
+    #"--headless",
+    #"--disable-gpu",
+    #"--window-size=1920,1200",
+    #"--ignore-certificate-errors",
+    #"--disable-extensions",
+    #"--no-sandbox",
+    #"--disable-dev-shm-usage",
+    #'--remote-debugging-port=9222'
+]
+
+for option in options:
+    chrome_options.add_argument(option)
+
+driver = webdriver.Chrome(options = chrome_options)
 
 website = "https://github.com"
 
 #chrome_options = Options()
 #driver = webdriver.Chrome(options=chrome_options)
-
-
 #options = webdriver.ChromeOptions()
-options = Options()
+#options = Options()
 #options.add_argument('--headless')
-options.headless = True
+#options.headless = True
 #driver = webdriver.Chrome()
-driver = webdriver.Chrome(options=options)
+#driver = webdriver.Chrome(options=options)
 driver.get(website)
 
 try:
