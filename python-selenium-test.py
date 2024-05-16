9610749066496ec9c8c58d72a9d1828724ad4fdb
Adrian Pastor <6169628+pagvac@users.noreply.github.com>
2024-05-15 23:01:01 -0400

    Update python-selenium-test.py

--- python-selenium-test.py
+++ python-selenium-test.py
@@ -1,17 +1,14 @@
-import time, random
-import chromedriver_autoinstaller
 from selenium import webdriver
-from pyvirtualdisplay import Display
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+import chromedriver_autoinstaller
+from pyvirtualdisplay import Display
+display = Display(visible=0, size=(800, 800))  
+display.start()
 
-#display = Display(visible=0, size=(800, 800))  
-#display.start()
-
-chromedriver_autoinstaller.install()
+chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
+                                      # and if it doesn't exist, download it automatically,
+                                      # then add chromedriver to path
 
 chrome_options = webdriver.ChromeOptions()    
 # Add your options as needed    
@@ -33,27 +30,10 @@ options = [
 for option in options:
     chrome_options.add_argument(option)
 
+    
 driver = webdriver.Chrome(options = chrome_options)
 
-website = "https://github.com"
-
-#chrome_options = Options()
-#driver = webdriver.Chrome(options=chrome_options)
-#options = webdriver.ChromeOptions()
-#options = Options()
-#options.add_argument('--headless')
-#options.headless = True
-#driver = webdriver.Chrome()
-#driver = webdriver.Chrome(options=options)
-driver.get(website)
-
-try:
-    driver.execute_script('document.getElementsByTagName("html")[0].style.scrollBehavior = "auto"')
-    driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/header/div/div[2]/div/div/div/a").click()
-    current_url = driver.current_url
-    WebDriverWait(driver, 15).until(EC.url_changes(current_url))
-    #time.sleep(random.randint(3, 6))
-    driver.save_screenshot('website.png')
-    driver.quit()
-except NoSuchElementException:
-    pass
+driver.get('http://github.com')
+print(driver.title)
+with open('./GitHub_Action_Results.txt', 'w') as f:
+    f.write(f"This was written with a GitHub action {driver.title}")
