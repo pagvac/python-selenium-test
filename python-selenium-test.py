100b070c8b900bef501894c5fed267e1d34043e1
Adrian Pastor <6169628+pagvac@users.noreply.github.com>
2024-05-15 22:37:14 -0400

    Create python-selenium-test.py

---
+++ python-selenium-test.py
@@ -0,0 +1,32 @@
+import time, random
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support import expected_conditions as EC
+
+website = "https://github.com"
+
+#chrome_options = Options()
+#driver = webdriver.Chrome(options=chrome_options)
+
+
+#options = webdriver.ChromeOptions()
+options = Options()
+#options.add_argument('--headless')
+options.headless = True
+#driver = webdriver.Chrome()
+driver = webdriver.Chrome(options=options)
+driver.get(website)
+
+try:
+    driver.execute_script('document.getElementsByTagName("html")[0].style.scrollBehavior = "auto"')
+    driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/header/div/div[2]/div/div/div/a").click()
+    current_url = driver.current_url
+    WebDriverWait(driver, 15).until(EC.url_changes(current_url))
+    #time.sleep(random.randint(3, 6))
+    driver.save_screenshot('website.png')
+    driver.quit()
+except NoSuchElementException:
+    pass
