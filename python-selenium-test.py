bfbd2e0e63cb049484f6ff38fdcfece8e231b064
Adrian Pastor <6169628+pagvac@users.noreply.github.com>
2024-05-15 22:49:07 -0400

    Update python-selenium-test.py

--- python-selenium-test.py
+++ python-selenium-test.py
@@ -1,10 +1,13 @@
 import time, random
+import chromedriver_autoinstaller
 from selenium import webdriver
+from pyvirtualdisplay import Display
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support import expected_conditions as EC
+
 
 website = "https://github.com"
 
